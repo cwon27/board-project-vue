2fbbdd475c90bd5dd1cc8c6fbb3ec9f95a998af8
cwon27 <sing2727@naver.com>
2025-02-26 09:37:41 +0900

    [25.02.26 09:37] 뷰 셋팅하고 스타일 적용중 commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<style>
+@import "./css/style.css";
+</style>
+
+<script>
+import FloatRight from "./components/layout/FloatRight.vue";
+import Menubar from "./components/layout/Menubar.vue";
+import BoardFooter from "./components/layout/BoardFooter.vue";
+import BoardHeader from "./components/layout/BoardHeader.vue";
+
+export default {
+  components: {
+    Header,
+    Menubar,
+    FloatRight,
+    Footer,
+  },
+};
+</script>
+
+<template>
+  <div class="wrap">
+    <BoardHeader />
+    <div class="container">
+      <Menubar />
+      <div className="contents">
+        <div class="location">
+          <span className="ic-home">HOME</span>
+          <span>커뮤니티</span>
+          <em>통합게시판</em>
+        </div>
+        <div className="tit-area">
+          <h3 className="h3-tit">통합게시판</h3>
+        </div>
+        <Outlet />
+      </div>
+    </div>
+    <FloatRight />
+    <BoardFooter />
+  </div>
+</template>
